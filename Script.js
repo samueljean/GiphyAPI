ab1d7a643312a5680f8a1804b3b889792a42d338
Sammy J <samueljean09@gmail.com>
2017-02-01 23:33:12 +0000

    work in progress

--- Script.js
+++ Script.js
@@ -13,15 +13,25 @@ $(document).ready(function() {
             // this is the jQuery way to create
             // a new image element
             var image = $('<img>');
+            var image2 = $('<img>');
             console.log(image);
             // when we create a new element,
             // jQuery returns an array. So we
             // need to look for the first item
             // in the image array
 
-            image[0].src = response.data[0].images.fixed_width_downsampled.url;
+            // image.attr('src', response.data[0].images.fixed_width_downsampled.url);
+            // image2.attr('src', response.data[1].images.fixed_width_downsampled.url);
+            /*for (var i; i<10;i=i + 1){
+                image.attr('src', response.data[i].images.fixed_width_downsampled.url);
+                $("#results").append(image);
+            }*/
+            for (var i=0; i< data.length; i++){
+               image.attr('src', response.data[i].images.fixed_width_downsampled.url)
+            }
             $("#results").empty();
             $("#results").append(image);
+            $("#results").append(image2);
         });
     });
 
